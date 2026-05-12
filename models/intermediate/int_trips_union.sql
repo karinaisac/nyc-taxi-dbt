d853ecf700028b66a929def70a981100a06f5473
Karina I <kisacenkova@gmail.com>
2026-05-12 10:49:45 +0000

    sync

--- models/intermediate/int_trips_union.sql
+++ models/intermediate/int_trips_union.sql
@@ -1,14 +1,36 @@
 with yellow_trips as (
-    select *,
-    null as trip_type, 
-    'yellow' as cab_type 
+    select
+        vendor_id,
+        pickup_datetime,
+        dropoff_datetime,
+        rate_code_id,
+        pickup_location_id,
+        dropoff_location_id,
+        passenger_count,
+        trip_distance,
+        fare_amount,
+        tip_amount,
+        total_amount,
+        cast(null as string) as trip_type,
+        'yellow' as cab_type
     from {{ ref('stg_yellow_trips_2022') }}
 ),
 
 green_trips as (
-    select *,
-    trip_type,
-    'green' as cab_type
+    select
+        vendor_id,
+        pickup_datetime,
+        dropoff_datetime,
+        rate_code_id,
+        pickup_location_id,
+        dropoff_location_id,
+        passenger_count,
+        trip_distance,
+        fare_amount,
+        tip_amount,
+        total_amount,
+        trip_type,
+        'green' as cab_type
     from {{ ref('stg_green_trips_2022') }}
 )
 
--- models/staging/stg_yellow_trips_2022.sql
+++ models/staging/stg_yellow_trips_2022.sql
@@ -8,24 +8,24 @@ select
     cast(vendor_id as int64) as vendor_id,
     pickup_datetime,
     dropoff_datetime,
-    cast(rate_code as int64) as rate_code,
+    cast(rate_code as int64) as rate_code_id,
     cast(pickup_location_id as int64) as pickup_location_id,
     cast(dropoff_location_id as int64) as dropoff_location_id,
     passenger_count,
     trip_distance,
     fare_amount,
     tip_amount,
-    total_amount
+    cast(total_amount as float64) as total_amount
 from source
 
     where trip_distance > 0
     and fare_amount > 0
-    and total_amount > 0
+    and cast(total_amount as float64) > 0
     and pickup_datetime < dropoff_datetime
     and passenger_count > 0
     and extract(year from pickup_datetime) = 2022
     and extract(year from dropoff_datetime) = 2022
-    and rate_code not in (0,99)
+    and cast(rate_code as int64) not in (0,99)
 )
 
 select * from cleaned_yellow_trips_2022
