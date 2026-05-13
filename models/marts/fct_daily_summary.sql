148b20b3b094d2ba6e3555e6c5d141fef7bfcbdc
Karina I <kisacenkova@gmail.com>
2026-05-13 12:15:39 +0000

    sync

--- models/marts/fct_daily_summary.sql
+++ models/marts/fct_daily_summary.sql
@@ -3,7 +3,6 @@ select * from {{ ref('int_trips_enriched') }}
 )
 
 select
-date(pickup_datetime) as trip_date,
 format_date('%Y-%m', date(pickup_datetime)) as trip_month,
 cab_type,
 sum(total_amount) as total_revenue,
@@ -15,4 +14,4 @@ sum(passenger_count) as total_passengers
 
 from source
 
-group by trip_date, trip_month, cab_type
+group by trip_month, cab_type
